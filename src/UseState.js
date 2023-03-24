980b739c749e5d061d0969802ed84a01972a0f4d
Gabriel LR <dayta.scientist@gmail.com>
2023-03-24 19:11:54 -0300

    Value status and Security code validation

--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 
+const SECURITY_CODE = "paradigma";
+
 const UseState = ({ name }) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onChangeValue = (event) => {
+    setValue(event.target.value);
+  };
 
   useEffect(() => {
     if (!!loading) {
       setTimeout(() => {
         console.log("Validacion");
-        setLoading(false);
+
+        if (value === SECURITY_CODE) {
+          setLoading(false);
+          setError(false);
+        } else {
+          setLoading(false);
+          setError(true);
+        }
       }, [2000]);
     }
   }, [loading]);
@@ -18,11 +32,19 @@ const UseState = ({ name }) => {
     <div>
       <h2>Delete {name}</h2>
       <p>Please, write the security code</p>
-
       {error && <p>Incorrect code</p>}
-
-      <input type="text" placeholder="security code" />
-      <button onClick={() => setLoading((prevState) => !prevState)}>
+      {loading && <p>Cargando ...</p>}
+      <input
+        type="text"
+        placeholder="security code"
+        value={value}
+        onChange={onChangeValue}
+      />
+      <button
+        onClick={() => {
+          setLoading(true);
+        }}
+      >
         Check it out
       </button>
     </div>
