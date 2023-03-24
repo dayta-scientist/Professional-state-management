d1c89df984b06d3be443042e09bdc45147613152
Gabriel LR <dayta.scientist@gmail.com>
2023-03-24 14:27:05 -0300

    Error Status

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
   render() {
     return (
       <div>
         <h2>Delete {this.props.name}</h2>
         <p>Please, write the security code</p>
+
+        {this.state.error && <p>Incorrect code</p>}
         <input type="text" placeholder="security code" />
-        <button>Check it out</button>
+        <button
+          onClick={() => this.state((prevState) => ({ error: !prevState }))}
+        >
+          Check it out
+        </button>
       </div>
     );
   }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 const UseState = ({ name }) => {
+  const [error, setError] = useState(true);
+
   return (
     <div>
       <h2>Delete {name}</h2>
       <p>Please, write the security code</p>
+
+      {error && <p>Incorrect code</p>}
+
       <input type="text" placeholder="security code" />
-      <button>Check it out</button>
+      <button onClick={() => setError((prevState) => !prevState)}>
+        Check it out
+      </button>
     </div>
   );
 };
